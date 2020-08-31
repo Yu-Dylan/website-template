e1ea08df277c6cd13cffe84b911d7ea967ed16e0
Dylan Yu <64719370+Yu-Dylan@users.noreply.github.com>
2020-08-31 16:22:08 -0500

    Update index.js

--- src/components/FirebaseCode/index.js
+++ src/components/FirebaseCode/index.js
@@ -11,14 +11,3 @@ import Firebase from './firebase';
 export default Firebase;
 
 export { FirebaseContext, withFirebase };
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCSv7oSPrAC-q9juqGQ0ZDp6SCvG3VIl2E",
-  authDomain: "dylan-tutoring.firebaseapp.com",
-  databaseURL: "https://dylan-tutoring.firebaseio.com",
-  projectId: "dylan-tutoring",
-  storageBucket: "dylan-tutoring.appspot.com",
-  messagingSenderId: "97639189537",
-  appId: "1:97639189537:web:b5177eeed03fb4ad636760",
-  measurementId: "G-SDS0T03EBR"
-};
